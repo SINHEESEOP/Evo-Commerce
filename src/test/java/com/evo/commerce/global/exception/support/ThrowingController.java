4b1575b4bfac50970154ae9392b55980af34970c
sinheeseop <shfurwnd97@gmail.com>
2026-08-27 14:14:44 +0900

    refactor: 테스트 더미 컨트롤러를 support 하위 패키지의 최상위 클래스로 재분리
    @Import로 중첩 클래스를 등록하는 방식도 동작은 하지만, 슬라이스 테스트가
    늘어날 때 재사용하기 어려워 최상위 클래스 + support 패키지 구조로 되돌림.
    ISSUE-03의 해결 방안도 실제 검증 결과에 맞게 수정.

--- src/test/java/com/evo/commerce/global/exception/support/ThrowingController.java
+++ src/test/java/com/evo/commerce/global/exception/support/ThrowingController.java
@@ -1,4 +1,4 @@
-package com.evo.commerce.global.exception;
+package com.evo.commerce.global.exception.support;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
